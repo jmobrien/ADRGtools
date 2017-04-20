9992120f90b10f74168c4dc4a1c51bba2438bc42
Joseph O'Brien <joseph.obrien@utexas.edu>
2017-04-20 16:12:17 -0500

    Notes.

--- R/package.functions.R
+++ R/package.functions.R
@@ -1,3 +1,6 @@
+
+# This function allows you to load packages and install them if they aren't there.  
+# when you have it, you just have to write get.packages("packageA", "packageB", "packageC", ..., etc.)
 get.packages <- function(...){
   packages <- list(...)
   invisible(
